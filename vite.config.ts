45d4dc208ace75e507e89e5e894c9896548957e4
easypie65 <easypie65@gmail.com>
2025-09-04 11:20:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/2025history/', // 리포 이름에 맞게!
   plugins: [react()],
-  base: '/2025history/'
 })
